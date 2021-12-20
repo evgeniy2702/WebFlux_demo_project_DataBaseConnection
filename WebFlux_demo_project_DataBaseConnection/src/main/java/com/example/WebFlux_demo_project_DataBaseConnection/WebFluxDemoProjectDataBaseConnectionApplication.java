b58e50cd63e4ffdde1ba70eea83164e93d63d228
evgeniy2702 <evgeniy,zhurenko@gmail.com>
2021-12-20 11:04:21 +0200

    Init project WebFlux + DataBaseConnection

---
+++ WebFlux_demo_project_DataBaseConnection/src/main/java/com/example/WebFlux_demo_project_DataBaseConnection/WebFluxDemoProjectDataBaseConnectionApplication.java
@@ -0,0 +1,13 @@
+package com.example.WebFlux_demo_project_DataBaseConnection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFluxDemoProjectDataBaseConnectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebFluxDemoProjectDataBaseConnectionApplication.class, args);
+	}
+
+}
---
+++ WebFlux_demo_project_DataBaseConnection/src/test/java/com/example/WebFlux_demo_project_DataBaseConnection/WebFluxDemoProjectDataBaseConnectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WebFlux_demo_project_DataBaseConnection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebFluxDemoProjectDataBaseConnectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
